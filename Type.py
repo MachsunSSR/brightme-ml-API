f342a8c9c316b5c78b9c4520fd603a8b67e097bf
MachsunSSR <safirmachsun@gmail.com>
2023-10-30 18:14:08 +0700

    update found bug

--- Type.py
+++ Type.py
@@ -33,8 +33,9 @@ model2 = tf.keras.models.load_model(
        custom_objects={'KerasLayer':hub.KerasLayer}
 )
 
-@application.route('/recomendation', methods = ['POST'])
+@application.route('/recomendation', methods = ['GET','POST'])
 def predict1():
+    print(request.files)
     if 'file' not in request.files:
         respond = jsonify({'message': 'No image'})
         respond.status_code = 400
@@ -104,77 +105,77 @@ def predict1():
             }
         )
 
-@application.route('/recomendation', methods = ['GET'])
-def predict2():
-    print(request)
-    if 'file' not in request.files:
-        respond = jsonify({'message': 'No image'})
-        respond.status_code = 400
-        return respond
-    files = request.files.getlist('file')
-    filename = "download.jpg"
-    errors = {}
-    success = False
-    for file in files:
-        if file and allowed_ex(file.filename):
-            file.save(os.path.join(application.config['UPLOAD_FOLDER'], file.filename))
-            success = True
-        else:
-            errors["message"] = 'The type of {} is wrong'.format(file.filename)
-    if not success:
-        resp = jsonify(errors)
-        resp.status_code = 400
-        return resp
+# @application.route('/recomendation', methods = ['GET'])
+# def predict2():
+#     print(request)
+#     if 'file' not in request.files:
+#         respond = jsonify({'message': 'No image'})
+#         respond.status_code = 400
+#         return respond
+#     files = request.files.getlist('file')
+#     filename = "download.jpg"
+#     errors = {}
+#     success = False
+#     for file in files:
+#         if file and allowed_ex(file.filename):
+#             file.save(os.path.join(application.config['UPLOAD_FOLDER'], file.filename))
+#             success = True
+#         else:
+#             errors["message"] = 'The type of {} is wrong'.format(file.filename)
+#     if not success:
+#         resp = jsonify(errors)
+#         resp.status_code = 400
+#         return resp
     
     
-    foto = os.path.join(application.config['UPLOAD_FOLDER'], file.filename)
-    img = keras.utils.load_img(foto, target_size=(224, 224))
-    x = tf.keras.utils.img_to_array(img)
-    x /= 255
-    x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
-    classes = model1.predict(images, batch_size=10)
-    classes2 = model2.predict(images, batch_size=10)
-    value2 = max(classes2[0])
-    value = max(classes[0])
-    i = 0
-    index_value=0
-    for result in classes[0]:
-        if result == value:
-            index_value = i
-        i += 1
-    skin_value = ""
-    if index_value == 0:
-        skin_value = "Acne"
-    elif index_value == 1:
-        skin_value = "Black Spots"
-    elif index_value == 2:
-        skin_value = "Puff Eyes"
-    elif index_value == 3:
-        skin_value = "Wrinkles"
-    print(classes2[0])
-    index = np.argmax(classes2[0])
-    print(index)
-    skin_value2 = ""
-    if index == 0:
-        skin_value2 = "Dry"
-    elif index == 1:
-        skin_value2 = "Normal"
-    elif index == 2:
-        skin_value2 = "Oily"
-    elif index == 3:
-        skin_value2 = "Sensitive"
+#     foto = os.path.join(application.config['UPLOAD_FOLDER'], file.filename)
+#     img = keras.utils.load_img(foto, target_size=(224, 224))
+#     x = tf.keras.utils.img_to_array(img)
+#     x /= 255
+#     x = np.expand_dims(x, axis=0)
+#     images = np.vstack([x])
+#     classes = model1.predict(images, batch_size=10)
+#     classes2 = model2.predict(images, batch_size=10)
+#     value2 = max(classes2[0])
+#     value = max(classes[0])
+#     i = 0
+#     index_value=0
+#     for result in classes[0]:
+#         if result == value:
+#             index_value = i
+#         i += 1
+#     skin_value = ""
+#     if index_value == 0:
+#         skin_value = "Acne"
+#     elif index_value == 1:
+#         skin_value = "Black Spots"
+#     elif index_value == 2:
+#         skin_value = "Puff Eyes"
+#     elif index_value == 3:
+#         skin_value = "Wrinkles"
+#     print(classes2[0])
+#     index = np.argmax(classes2[0])
+#     print(index)
+#     skin_value2 = ""
+#     if index == 0:
+#         skin_value2 = "Dry"
+#     elif index == 1:
+#         skin_value2 = "Normal"
+#     elif index == 2:
+#         skin_value2 = "Oily"
+#     elif index == 3:
+#         skin_value2 = "Sensitive"
    
-    return jsonify(
-         {
-                "status": "Success",
-                "message": "Successfully making prediction",
-                "data": {
-                    "input1": skin_value,
-                    "input2": skin_value2,
-                }
-            }
-        )
+#     return jsonify(
+#          {
+#                 "status": "Success",
+#                 "message": "Successfully making prediction",
+#                 "data": {
+#                     "input1": skin_value,
+#                     "input2": skin_value2,
+#                 }
+#             }
+#         )
 
 if __name__ == "__main__":
     
